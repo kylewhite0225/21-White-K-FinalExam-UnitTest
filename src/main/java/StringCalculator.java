4426f67e0687038be71123ca4971191ec7a8ab15
Kyle White <kylewhite0225@gmail.com>
2021-06-23 13:08:34 -0700

    Added method implementations to pass unit test for negative throw exception and numbers over 1000.

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -3,6 +3,12 @@ public class StringCalculator {
         int returnValue = 0;
         String[] numbersArray = numbers.split(",|\\n");
         for (String number : numbersArray) {
+            if(Integer.parseInt(number.trim()) < 0) {
+                throw new IllegalArgumentException("Cannot contain negative numbers.");
+            }
+            if(Integer.parseInt(number.trim()) > 1000) {
+                continue;
+            }
             if (!number.trim().isEmpty()) {
                 returnValue += Integer.parseInt(number.trim());
             }
