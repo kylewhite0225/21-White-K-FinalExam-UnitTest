5a188e4f71285de3436ae2e070ffa0a43ed2aa13
Kyle White <kylewhite0225@gmail.com>
2021-06-23 13:01:51 -0700

    updated maven dependencies, created test class

---
+++ src/test/java/StringCalculatorTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+
+    @Test
+    public final void negativeThrowException() {
+        Exception e = assertThrows(IllegalArgumentException.class, () -> {
+            StringCalculator.add("-1, 2, 3");
+        });
+        assertTrue(e.getMessage().contains("Cannot contain negative numbers."));
+    }
+
+    @Test
+    public final void ignoreOver1000() {
+        int expected = 10;
+        int actual = StringCalculator.add("10, 1001");
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public final void testStringCalc() {
+        int expected = 10;
+        int actual = StringCalculator.add("3, 7");
+        assertEquals(expected, actual);
+    }
+
+}
